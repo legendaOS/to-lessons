ed6c564e32f50c89004ad08d38408fc865b491be
FedOS <76245715+legendaOS@users.noreply.github.com>
2023-02-07 17:44:48 +0300

    добавлено описание и решение задачи 7

---
+++ task-7-if/main.py
@@ -0,0 +1,10 @@
+# Задача 7
+
+if __name__ == '__main__':
+
+    n = int(input("Введите целое число: "))
+
+    if (n % 2 == 0) and (n % 3 == 0):
+        print("TRUE-YES")
+    else:
+        print("FALSE-N")
